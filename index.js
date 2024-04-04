820997e1d75cd8f669361450bbc72d9236ff5743
megahert69 <megahert101@gmail.com>
2024-04-04 10:26:01 -0700

    updated Javascript and API

---
+++ index.js
@@ -0,0 +1,42 @@
+const API_KEY = "9877979a213d4cf18c967647477be931";
+const recipeListEl = document.getElementById('recipe-list')
+
+function displayRecipes(recipes){
+    recipeListEl.innerHTML = "";
+    recipes.forEach((recipe) => {
+        const recipeItemEl = document.createElement('li');
+        const recipeImageEl = document.createElement('img');
+        const recipeTitleEl = document.createElement('h2');
+        const recipeIngEl = document.createElement('p');
+
+        recipeIngEl.innerHTML = `
+            <strong>Ingredients:</strong> ${recipe.extendedIngredients.
+                                          map((ing) => ing.original).join(', ')}`;
+        recipeTitleEl.innerText = recipe.title;
+        recipeItemEl.classList.add('recipe-item');
+        recipeImageEl.src = recipe.image;
+        recipeImageEl.alt = "recipe";
+
+        recipeItemEl.appendChild(recipeImageEl);
+        recipeItemEl.appendChild(recipeTitleEl);
+        recipeItemEl.appendChild(recipeIngEl);
+        recipeListEl.appendChild(recipeItemEl);
+    });
+}
+
+async function getRecipes(){
+    const response = await fetch(`https://api.spoonacular.com/recipes/random?number=10&apiKey=${API_KEY}`)
+
+    const data = await response.json()
+
+    return data.recipes
+}
+
+async function init(){
+    const recipes = await getRecipes();
+    // console.log(recipes)
+
+    displayRecipes(recipes)
+}
+
+init();
